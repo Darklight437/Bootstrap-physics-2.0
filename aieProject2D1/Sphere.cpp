2ba7fdbd1136fa596d37efaa5e102020609c90b3
Darklight437 <darklight437@gmail.com>
2018-02-12 16:28:17 +1100

    dll machine broke, understandable have a good day

--- aieProject2D1/Sphere.cpp
+++ aieProject2D1/Sphere.cpp
@@ -31,22 +31,22 @@ void Sphere::makeGizmo()
 
 bool Sphere::checkCollision(PhysicsObject * pOther)
 {
-	glm::vec2 otherPos;
-	float otherRad;
-	if (dynamic_cast<Sphere*>(pOther) != nullptr)
-	{
-		otherPos = dynamic_cast<Sphere*>(pOther)->getPosition();
-		otherRad = dynamic_cast<Sphere*>(pOther)->getRadius();
-	}
-	glm::vec2 distanceBetween = m_position - otherPos;
-	
-	if (glm::distance( m_position , otherPos) <= (m_radius + otherRad))
-	{
-		glm::vec2 separateTeleport(glm::normalize(distanceBetween).x * 0.1 , 0);
-		std::cout << glm::distance(m_position, otherPos) << std::endl;
-		dynamic_cast<Sphere*>(pOther)->m_position -= separateTeleport;
-		return true;
-	}
+	//glm::vec2 otherPos;
+	//float otherRad;
+	//if (dynamic_cast<Sphere*>(pOther) != nullptr)
+	//{
+	//	otherPos = dynamic_cast<Sphere*>(pOther)->getPosition();
+	//	otherRad = dynamic_cast<Sphere*>(pOther)->getRadius();
+	//}
+	//glm::vec2 distanceBetween = m_position - otherPos;
+	//
+	//if (glm::distance( m_position , otherPos) <= (m_radius + otherRad))
+	//{
+	//	glm::vec2 separateTeleport(glm::normalize(distanceBetween).x * 0.1 , 0);
+	//	std::cout << glm::distance(m_position, otherPos) << std::endl;
+	//	dynamic_cast<Sphere*>(pOther)->m_position -= separateTeleport;
+	//	return true;
+	//}
 
-	return false;
+	//return false;
 }
