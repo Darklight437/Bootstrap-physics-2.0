2dae8695e424a3d18b67501806a0f4db36cc9d62
Darklight437 <darklight437@gmail.com>
2018-02-26 15:53:41 +1100

    fix AABB collisions next

--- aieProject2D1/aieProject2D1App.cpp
+++ aieProject2D1/aieProject2D1App.cpp
@@ -48,7 +48,7 @@ bool aieProject2D1App::startup()
 	//m_PhysicsScene->addActor(new Sphere(glm::vec2(-40, 0), glm::vec2(15, 0), 1.0f, 5.0f, 0.3f, glm::vec4(1, 0, 1, 1)));
 	
 	
-	//m_PhysicsScene->addActor(new Box(glm::vec2(40, 0), glm::vec2(-10, 0), glm::vec2(6, 6), 1.0f, glm::vec4(0, 0, 1, 1)));
+	m_PhysicsScene->addActor(new Box(glm::vec2(-40, 0), glm::vec2(-10, 0), glm::vec2(6, 6), 1.0f, glm::vec4(0, 0, 1, 1)));
 
 	
 	//setupContinuousDemo(glm::vec2(-40,0),20, 9.8, -9.8);
