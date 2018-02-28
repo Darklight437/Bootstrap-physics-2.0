1fab5cb5d9e8f769ca010c3c92e3cb83acb2fe23
Darklight437 <darklight437@gmail.com>
2018-02-28 12:26:05 +1100

    polygon data thingos

--- aieProject2D1/Polygon.h
+++ aieProject2D1/Polygon.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "RigidBody.h"
+#include <vector>
 class Polygon :
 	public RigidBody
 {
@@ -7,9 +8,18 @@ public:
 	Polygon(glm::vec2 position, glm::vec2 velocity, float mass);
 	~Polygon();
 
+	glm::vec2 getpoint(int);
+
+protected:
 
 
+	void calculateNormals();
 
 
+	std::vector<glm::vec2> points;
+	std::vector<glm::vec2> normals;
+
+	std::vector<glm::vec2> localPoint;
+	std::vector<glm::vec2> localNormals;
 };
 
